13ae7729b20cf83794b23a1b9c2cccfb19f15723
connquestt <ksakhalod424@gmail.com>
2026-01-17 18:23:19 +0300

    Dart test

---
+++ bin/dart_kh.dart
@@ -0,0 +1,5 @@
+import 'package:dart_kh/dart_kh.dart' as dart_kh;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_kh.calculate()}!');
+}
---
+++ test/dart_kh_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_kh/dart_kh.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
