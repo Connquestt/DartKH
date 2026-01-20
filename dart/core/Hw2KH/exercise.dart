f016b21b84d9c4fb4573a61c62e0d51bb6bf45f9
connquestt <ksakhalod424@gmail.com>
2026-01-20 16:34:10 +0300

    Day two Hw!

---
+++ dart/core/Hw2KH/exercise.dart
@@ -0,0 +1,47 @@
+class Person {
+  String name;
+  String email;
+
+  Person(this.name, this.email);
+}
+
+class Author extends Person {
+  static int idCount = 1;
+  int id = idCount;
+
+  Author(super.name, super.email) {
+    id = idCount;
+    idCount++;
+  }
+}
+
+class Book {
+  String name;
+  String ISBN;
+  int editionNumber;
+  Author author;
+  Book({
+    required this.name,
+    required this.ISBN,
+    required this.editionNumber,
+    required this.author,
+  });
+
+  @override
+  String toString() {
+    return ('Book name: $name \n Book Author and id: ${author.name} ${author.id}\n');
+  }
+}
+
+class Customer extends Person {
+  List<Book> bookList = [];
+
+  Customer(super.name, super.email);
+
+  void addBook(Book book) => bookList.add(book);
+
+  void printInformation() {
+    print("Customer Name : $name \n Customer email: $email \n");
+    print(bookList);
+  }
+}
---
+++ dart/core/Hw2KH/main.dart
@@ -0,0 +1,33 @@
+import "exercise.dart";
+
+void main() {
+  Author author1 = Author("KHalid", "KHalid@k.com");
+  Author author2 = Author("Muhannd", "Muhannd@k.com");
+  Author author3 = Author("Naif", "Naif@k.com");
+
+  Book book1 = Book(
+    name: "How to become good at Programming",
+    ISBN: 'K29643',
+    editionNumber: 1,
+    author: author1,
+  );
+  Book book2 = Book(
+    name: "How to become bad at Programming",
+    ISBN: 'M29643',
+    editionNumber: 1,
+    author: author2,
+  );
+  Book book3 = Book(
+    name: "How to meh at Programming",
+    ISBN: 'N29643',
+    editionNumber: 1,
+    author: author3,
+  );
+
+  Customer customer1 = Customer("Customer1", "Customer@library");
+  customer1.addBook(book1);
+  customer1.addBook(book2);
+  customer1.addBook(book3);
+
+  customer1.printInformation();
+}
